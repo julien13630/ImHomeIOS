bcff344791f3650d98a2d49d334460d0988e77bb
Julien <julien@iMac-de-Julien.local>
2017-06-02 19:14:01 +0200

    Initial Commit

---
+++ ImHomeIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImHomeIOS
+//
+//  Created by Julien on 02/06/2017.
+//  Copyright © 2017 Julien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
